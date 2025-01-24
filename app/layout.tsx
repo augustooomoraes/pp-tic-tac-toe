ae877f3c4829c9e8a368d5c95c64164297105b16
Augusto Moraes <cdm.augusto@gmail.com>
2025-01-24 15:50:43 -0300

    upd opengraph metadata before releasing in augustooomoraes.com

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,11 +10,11 @@ const montserrat = Montserrat({ weight: ["400", "500"], subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Jogo da velha",
   description: "Portfólio de desenvolvimento web. Veja mais em augustooomoraes.com. Jogável em abas distintas. Dados das partidas guardados no navegador.",
-  metadataBase: new URL("https://pp-tic-tac-toe.vercel.app/"),
+  metadataBase: new URL("https://tictactoe.augustooomoraes.com/"),
   openGraph: {
     images: [
       {
-        url: "https://pp-tic-tac-cwleunyr6-augustomoraes.vercel.app/opengraph-image.png?65079fb8918cb5d6=",
+        url: "/opengraph-image.png",
         width: 800,
         height: 400,
         alt: "Augusto Moraes' Tic Tac Toe portfolio website project cover image.",
@@ -22,9 +22,6 @@ export const metadata: Metadata = {
     ],
   },
 };
-
-// Há a aba 'Open Graph' nas páginas dos deployments, nas páginas das publicações, no Vercel. Ver se há qualquer pista ali sobre essa confusão toda.
-// https://vercel.com/augustomoraes/pp-tic-tac-toe/deployments
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
